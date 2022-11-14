b834fd35dae1effad79df9767858eb56f1ee5c95
Andreas Karlsson <andreas@jhkarlsson.com>
2022-11-14 19:41:46 +0100

    Add user notification for permissions issue

--- MacDial/AppDelegate.swift
+++ MacDial/AppDelegate.swift
@@ -1,5 +1,6 @@
 
 import Cocoa
+import ServiceManagement
 import SwiftUI
 
 @main
@@ -7,8 +8,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var statusBarController: StatusBarController?
     let dial = Dial()
+    
+    func requestPermissions() {
+        // More information on this behaviour: https://stackoverflow.com/questions/29006379/accessibility-permissions-reset-after-application-update
+        if !AXIsProcessTrusted() {
+            let alert = NSAlert()
+            alert.messageText = "App permissions"
+            alert.alertStyle = NSAlert.Style.informational
+            alert.informativeText = "Mac Dial needs Accessibility permissions to work. In the next dialog you will be asked to open the Settings app to enable the permissions.\nIMPORTANT! Due to an issue in macOS, if you're upgrading from an earlier version of Mac Dial you might have to remove Mac Dial from the accessibility permissions and then restart the app to re-add the permissions."
+            alert.runModal()
+        }
+        
+        let options : NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true]
+        
+        AXIsProcessTrustedWithOptions(options)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        requestPermissions()
         dial.start();
         statusBarController = StatusBarController.init(dial)
     }
